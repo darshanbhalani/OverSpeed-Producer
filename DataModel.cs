c1d374e6ea882edb2174c6fbd1097648e043d144
DarshanBhalani-Amnex <165637028+DarshanBhalani-Amnex@users.noreply.github.com>
2024-05-29 12:53:57 +0530

    Add project files.

---
+++ DataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Kafka_Producer
+{
+    internal class DataModel
+    {
+        public string VehicleNumber {get; set;}
+        public int VehicleSpeed { get; set;}  
+        public DateTime TimeStamp { get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using Confluent.Kafka;
+using Kafka_Producer;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+class Program
+{
+    private static List<string> vehicleNumber = ["GJ18AB1245", "GJ18WR5535", "GJ18KZ1147", "GJ18WW0193", "GJ18UL1925", "GJ18DQ1845", "GJ18WR5535", "GJ18RS9505", "GJ18GP0853", "GJ18AB2305", "GJ18PL7852", "GJ18MR6548", "GJ18YN4379", "GJ18QR7261", "GJ18KT3190", "GJ18FS5017", "GJ18LS2834", "GJ18GH9912", "GJ18VC8745", "GJ18TN2103", "GJ18BP6490", "GJ18DZ1402", "GJ18RM3627", "GJ18YT5348", "GJ18HJ9821", "GJ18AB6453", "GJ18NM2084", "GJ18FW9172", "GJ18ZU1839", "GJ18CL7062"];
+    
+    public static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var config = new ProducerConfig
+        {
+            BootstrapServers = $"{configuration["BootstrapService:Server"]}:{configuration["BootstrapService:Port"]}"
+        };
+
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            while (true)
+            {
+                List<DataModel> dataModels = new List<DataModel>();
+
+                foreach (var v in vehicleNumber)
+                {
+                    dataModels.Add(new DataModel
+                    {
+                        VehicleNumber = v,
+                        VehicleSpeed = randomSpeed(),
+                        TimeStamp = DateTime.Now,
+                    });
+                }
+
+                try
+                {
+                    var deliveryResult = await producer.ProduceAsync(configuration["BootstrapService:Topic"], new Message<Null, string> { Value = JsonConvert.SerializeObject(dataModels) });
+                    await Console.Out.WriteLineAsync(JsonConvert.SerializeObject(dataModels));
+                    await Console.Out.WriteLineAsync();
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+       
+    }
+    private static int randomSpeed()
+    {
+        int max = 80;
+        Random random = new Random();
+        return random.Next(0, max + 1);
+    }
+}
